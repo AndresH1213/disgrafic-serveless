2e53cab0dbd1b1bf0400946b5906f3040463d63b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-18 23:54:26 -0500

    get all clientes route

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/clients/:clientId", require("./getClient"));
+
+app.get("/clients", require("./getAllClient"));
 
 app.post("/clients", require("./createClient"));
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: "Ruta no encontrada",
   });
 });
 
