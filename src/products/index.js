8b4c1963b62195315c671047f2e40dcd7f33527b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-18 23:53:24 -0500

    cors headers seting

--- src/products/index.js
+++ src/products/index.js
@@ -1,20 +1,34 @@
-require("dotenv").config();
-const express = require("express");
-const serverless = require("serverless-http");
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const serverless = require('serverless-http');
 
 const app = express();
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  next();
+});
+
 app.use(express.json());
 
-app.get("/products", require("./getProds"));
+app.get('/products', require('./getProds'));
 
-app.post("/products", require("./createProds"));
+app.post('/products', require('./createProds'));
 
-app.put("/products/:prodId", require("./updateProds"));
+app.post('/calculate', require('./calculate'));
+
+app.put('/products/:prodId', require('./updateProds'));
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: 'Not Found',
   });
 });
 
